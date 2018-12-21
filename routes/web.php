2fe4700feda40c863ae3346ee735cb98631d9506
ADETUNJI OPAYELE <lordwhizy@gmail.com>
2018-12-21 23:57:57 +0100

    Add project unit test

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ Route::get('/', function () {
 });
 
 Route::get('/projects', 'ProjectsController@index');
+Route::get('/projects/{project}', 'ProjectsController@show');
 
 Route::post('/projects', 'ProjectsController@store');
+
 
